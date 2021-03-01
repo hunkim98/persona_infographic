5203676803c3ac5fb4ae0dd521b474b55a69498b
Kim Dong Hun <hunkim98@gmail.com>
2021-03-01 23:20:12 +0900

    New data

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
-const database = new Request("http://localhost:3000/gatherData");
+const database = new Request("https://personapersonality.com/gatherData");
+// https://personapersonality.com/gatherData
 let user_data;
 
 fetch(database)
@@ -6,11 +7,18 @@ fetch(database)
   .then((data) => (user_data = data))
   .then(() => {
     for (i = 0; i < user_data.length; i++) {
+      const data_list = document.createElement("div");
+      data_list.className = "data_list";
       const data_name = document.createElement("div");
       data_name.innerHTML = user_data[i].name;
       const data_personality = document.createElement("div");
       data_personality.innerHTML = user_data[i].personality;
-      document.body.appendChild(data_name);
-      document.body.appendChild(data_personality);
+      const data_timestamp = document.createElement("div");
+      data_timestamp.innerHTML = user_data[i].timestamp;
+      data_list.appendChild(data_name);
+      data_list.appendChild(data_personality);
+      data_list.appendChild(data_timestamp);
+      document.body.appendChild(data_list);
     }
+    document.querySelector(".total_users .amount").innerHTML = user_data.length;
   });
